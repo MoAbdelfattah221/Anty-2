2e2f7991b73f3978bb6a029da9580bb10c0b8b5d
Mohamed Abdelfattah <99327472+MoAbdelfattah221@users.noreply.github.com>
2022-12-20 23:46:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,4 +7,43 @@ menuIcon.onclick = () => {
     cancelIcon.onclick = () => {
         menu.classList.toggle("active");
     };
+};     
+
+// Swiper Script
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// Statistics Counter
+let num = document.querySelectorAll(".num");
+let section = document.querySelector(".statistics");
+let started = false;
+window.onscroll = () => {
+    if(window.scrollY >= section.offsetTop){
+        if(!started){
+            num.forEach((num) => startCount(num));
+        };
+        started = true;
+    };
 };
+function startCount(el){
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if(el.textContent == goal){
+            clearInterval(count);
+        };
+    },2000 / goal)
+};
+
+// Footer 
+let date = new Date();
+let to = document.querySelector(".to");
+to.textContent = date.getFullYear();
